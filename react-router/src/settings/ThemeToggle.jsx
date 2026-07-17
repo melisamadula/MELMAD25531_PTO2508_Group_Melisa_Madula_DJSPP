0b1c55dda0bbc5d8a9ee91f6f2944e9a04c2f5d7
melisamadula <melisamadula.mm@gmail.com>
2026-07-17 23:42:51 +0200

    Styled the them toggle button itself

--- react-router/src/settings/ThemeToggle.jsx
+++ react-router/src/settings/ThemeToggle.jsx
@@ -1,12 +1,12 @@
 import { useTheme } from "./useTheme";
-import "./Theme.module.css";
+import styles from "./Theme.module.css";
 
 export const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme}>
-      Switch to {theme === 'light' ? '🌑 Dark' : '☀️ Light'} Mode
+    <button className={styles.toggleButton} onClick={toggleTheme}>
+      {theme === 'light' ? '🌑 Dark Mode' : '☀️ Light Mode'}
     </button>
   );
 };
